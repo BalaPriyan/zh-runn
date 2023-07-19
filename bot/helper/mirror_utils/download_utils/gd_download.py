3f5f4a7c6b2ebd79dde5ca3558374d5abac7d3e1
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-07-19 03:34:10 -0700

    Update gd_download.py

--- bot/helper/mirror_utils/download_utils/gd_download.py
+++ bot/helper/mirror_utils/download_utils/gd_download.py
@@ -1,17 +1,14 @@
+#!/usr/bin/env python3
 from random import SystemRandom
 from string import ascii_letters, digits
 
-from bot import (LOGGER, download_dict, download_dict_lock, non_queued_dl,
-                 queue_dict_lock)
-from bot.helper.ext_utils.bot_utils import sync_to_async
-from bot.helper.ext_utils.task_manager import (is_queued, limit_checker,
-                                               stop_duplicate_check)
+from bot import download_dict, download_dict_lock, LOGGER, non_queued_dl, queue_dict_lock
+from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.mirror_utils.status_utils.gdrive_status import GdriveStatus
 from bot.helper.mirror_utils.status_utils.queue_status import QueueStatus
-from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.telegram_helper.message_utils import (delete_links,
-                                                      sendMessage,
-                                                      sendStatusMessage)
+from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMessage
+from bot.helper.ext_utils.bot_utils import sync_to_async
+from bot.helper.ext_utils.task_manager import is_queued, limit_checker, stop_duplicate_check
 
 
 async def add_gd_download(link, path, listener, newname):
@@ -20,17 +17,16 @@ async def add_gd_download(link, path, listener, newname):
     if mime_type is None:
         await sendMessage(listener.message, name)
         return
+
     name = newname or name
     gid = ''.join(SystemRandom().choices(ascii_letters + digits, k=12))
 
     msg, button = await stop_duplicate_check(name, listener)
     if msg:
         await sendMessage(listener.message, msg, button)
-        await delete_links(listener.message)
         return
     if limit_exceeded := await limit_checker(size, listener, isDriveLink=True):
         await sendMessage(listener.message, limit_exceeded)
-        await delete_links(listener.message)
         return
     added_to_queue, event = await is_queued(listener.uid)
     if added_to_queue:
@@ -51,7 +47,7 @@ async def add_gd_download(link, path, listener, newname):
     drive = GoogleDriveHelper(name, path, listener)
     async with download_dict_lock:
         download_dict[listener.uid] = GdriveStatus(
-            drive, size, listener.message, gid, 'dl', listener.extra_details)
+            drive, size, listener.message, gid, 'dl', listener.upload_details)
 
     async with queue_dict_lock:
         non_queued_dl.add(listener.uid)
